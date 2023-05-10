68d336be7885a7216393f2e179a62dc58acd5835
jumagaliev1 <200103311@stu.sdu.edu.kz>
2023-05-10 18:21:56 +0600

    Create Swagger doc

--- src/main/java/com/github/jumagaliev1/booktopia/BooktopiaApplication.java
+++ src/main/java/com/github/jumagaliev1/booktopia/BooktopiaApplication.java
@@ -2,13 +2,19 @@ package com.github.jumagaliev1.booktopia;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 @SpringBootApplication
 public class BooktopiaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BooktopiaApplication.class, args);
 	}
-
 }
+
